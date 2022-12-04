e12ccd7e1e54802f73b0ca3e789a56ecbda50d13
John321Blacksmith <kuzmichevi321@yandex.ru>
2022-12-04 16:30:09 +0300

    the selection func was added

--- data_manager/database_manager.py
+++ data_manager/database_manager.py
@@ -27,7 +27,7 @@ def connect_to_the_db(filename):
 		return connection
 
 
-def insert_data(conn, query, objects):
+def insert_data(conn, query, objects, tables):
 
 	# create a cursor
 	cursor = conn.cursor()
@@ -50,7 +50,7 @@ def insert_data(conn, query, objects):
 		# fix all the changes
 		conn.commit()
 
-		print('The data hass been successfully loaded to the database.')
+		print('The data has been successfully loaded to the database.')
 
 	except (Exception, psycopg2.DatabaseError) as error:
 		print(ERROR + str(error))
@@ -60,16 +60,51 @@ def insert_data(conn, query, objects):
 			conn.close()
 
 
-def retrieve_data(conn, query):
+def retrieve_data(conn, query, table):
+	"""This function fetches the data from the table and
+	returns a list of objectsto be rendered in the django template."""
 
-	# create a cursor
-	cursor = conn.cursor()
+	objs = []
+	try:
+		# create a cursor
+		cursor = conn.cursor()
 
-	# pass
-	pass
+		
+		# operate the query 
+		cursor.execute(query.format('product_name', 'product_price', 'product_link', 'product_image', table))
 
+		# get a number of rows to be iterated
+		rows_amount = cursor.rowcount
 
+		# pass through the rows and get the values 
+		# from each and define an object
+		for i in range(0, rows_amount):
 
+			# extract a table record
+			row = cursor.fetchone()
+
+			# define an object (the product id is by default on the zero index)
+			obj = {
+				'title': row[0],
+				'price': row[1],
+				'link': row[2],
+				'image': row[3]
+			}
+
+			# put the object to the list
+			objs.append(obj)
+
+		# shut down the cursor
+		cursor.close()
+
+	except (Exception, psycopg2.DatabaseError) as error:
+		print(ERROR + str(error))
+
+	finally:
+		if conn is not None:
+			conn.close()
+
+	return objs
 
 def create_table(conn, query, tables):
 
@@ -95,6 +130,14 @@ def create_table(conn, query, tables):
 		if conn is not None:
 			conn.close()
 
+
+def update_data(conn, query, tables):
+	"""this function updates the data in the table."""
+
+	cursor = conn.cursor()
+
+	for table in tables:
+		pass
 
 def delete_data():
 	pass
