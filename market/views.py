b0ca9dba8314c0b75c85fce66c45942a7e401a64
John321Blacksmith <kuzmichevi321@yandex.ru>
2022-12-06 15:53:37 +0300

    templates have been added

--- market/views.py
+++ market/views.py
@@ -1,10 +1,27 @@
-import psycopg2
+import sys
+from . import secrs
 from django.shortcuts import render
-# Create your views here.
 
+sys.path.append(secrs.project_location)
+
+from logic_side.data_manager import database_manager
+
+# Create your views here.
 
 
 def index(request):
 	"""This function renders a main page of the market."""
 
-	return render(request, 'market/index.html')
+	 # a selection query command
+	selection_query = """SELECT {0}, {1}, {2}, {3} FROM {4};"""
+
+	# create a connection object
+	connection = database_manager.connect_to_the_db(secrs.params_location)
+
+	# get data from the storage
+	objects = database_manager.retrieve_data(connection, selection_query, 'food')
+
+	# create a context
+	context = {'products': objects}
+
+	return render(request, 'market/index.html', context)
