2390b79f409b798c332a2ebb21f2a28c8383cc3a
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-03-15 21:47:15 +0300

    menu models have been integrated

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Menu, MenuEntity, SubMenuEntity
 # Register your models here.
+
+admin.site.register(Menu)
+admin.site.register(MenuEntity)
+admin.site.register(SubMenuEntity)
