176e5e5f708a1909dda6a83de68177f63edaa682
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-06-17 12:56:22 +0300

    the projecthas been refined

--- menu/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Menu, MenuEntity, SubMenuEntity
-# Register your models here.
-
-admin.site.register(Menu)
-admin.site.register(MenuEntity)
-admin.site.register(SubMenuEntity)
