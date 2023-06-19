8e798adbb56406cf6f0b998ab273a2a5549b0e6b
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-06-19 16:37:20 +0300

    fixed

--- grocery/admin.py
+++ grocery/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Vegetables, Fruits
+from .models import Vegetable, Fruit
 # Register your models here.
 
 
-admin.site.register(Vegetables)
-admin.site.register(Fruits)
+admin.site.register(Vegetable)
+admin.site.register(Fruit)
